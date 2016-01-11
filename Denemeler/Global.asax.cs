5b52f54a7f5db30f22be7dbd377b92fcd645a052
mtswaredev <mtswaredev@gmail.com>
2016-01-11 11:44:02 +0200

    dsdasa

--- Denemeler/Global.asax.cs
+++ Denemeler/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+// bu ilk denemeydi M.U.
 namespace Denemeler
 {
     public class WebApiApplication : System.Web.HttpApplication
